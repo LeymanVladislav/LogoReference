0f20334a82d0335535bab84a77416e2459460fea
Vlad <leyman.vladislav@gmail.com>
2018-04-30 17:47:33 +0700

    Commit From LV PC

--- src/main/java/app/db/SQLiteJDBCDriverConnection.java
+++ src/main/java/app/db/SQLiteJDBCDriverConnection.java
@@ -9,13 +9,15 @@ import java.util.ArrayList;
  */
 public class SQLiteJDBCDriverConnection {
     private static String PKG_NAME = "SQLiteJDBCDriverConnection";
-    private static String url = "jdbc:postgres://"
-                                //+ "jrcyfbsexdmqnt:2f4a45cef0abbfe16df17d93565ab53d16e2b4994845a77fae3f01c0c7635f07@"
-                                //+ "ec2-79-125-117-53.eu-west-1.compute.amazonaws.com:5432/d40963l0827185";
-                                //+ "?sslmode=require";
-                                + "localhost:5432/postgres";
-    private static String USER = "sys"; //"jrcyfbsexdmqnt";
-    private static String PASS = "19871214"; //"2f4a45cef0abbfe16df17d93565ab53d16e2b4994845a77fae3f01c0c7635f07";
+    private static String DBDriver = "jdbc:postgresql";
+    private static String Host = "ec2-79-125-117-53.eu-west-1.compute.amazonaws.com";
+    private static String Port = "5432";
+    private static String DBName = "d40963l0827185";
+    private static String SSLMode = "sslmode=require";
+    private static String User = "jrcyfbsexdmqnt";
+    private static String Pass = "2f4a45cef0abbfe16df17d93565ab53d16e2b4994845a77fae3f01c0c7635f07";
+    private static String url = DBDriver + "://" + Host + ":" + Port + "/" + DBName + "?" + SSLMode;
+                                //"jdbc:postgresql://localhost:5432/postgres";
     private static Connection conn = null;
     /**
      * Connect to a sample database
@@ -29,7 +31,7 @@ public class SQLiteJDBCDriverConnection {
             DriverManager.registerDriver(new org.postgresql.Driver());
             //Class.forName("org.postgresql.Driver");
             // create a connection to the database
-            conn = DriverManager.getConnection(url, USER, PASS);
+            conn = DriverManager.getConnection(url, User, Pass);
 
             System.out.println(PKG_NAME + "." + Modul + "Connection to SQLite has been established.");
 
